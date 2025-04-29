ed2ff0158c97d0918c35cd834eb9ae4016220ca7
EA <ealymbaev@gmail.com>
2025-04-29 11:49:53 +0600

    Update `stellar-ios-mac-sdk` dependency

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "git@github.com:Soneso/stellar-ios-mac-sdk.git", .upToNextMajor(from: "3.0.7")),
+        .package(url: "https://github.com/Soneso/stellar-ios-mac-sdk.git", .upToNextMajor(from: "3.0.8")),
         .package(url: "https://github.com/groue/GRDB.swift.git", .upToNextMajor(from: "6.0.0")),
         .package(url: "https://github.com/horizontalsystems/HsToolKit.Swift.git", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/horizontalsystems/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.6")),
