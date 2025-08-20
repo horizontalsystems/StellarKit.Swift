d554a65cd4d011b5c309951e940185fed3de8070
Anton Stavnichiy <anton.stavnichiy@gmail.com>
2025-08-20 16:22:15 +0600

    Implement send methods for all_bridge stellar

--- Sources/StellarKit/Api/IApi.swift
+++ Sources/StellarKit/Api/IApi.swift
@@ -5,6 +5,7 @@ import stellarsdk
 protocol IApi {
     func getAccountDetails(accountId: String) async throws -> Account?
     func getOperations(accountId: String, from cursor: String?, asc: Bool, limit: Int) async throws -> [TxOperation]
+    func send(keyPair: KeyPair, transaction: Transaction) async throws -> String
     func sendTransaction(keyPair: KeyPair, operations: [stellarsdk.Operation], memo: Memo?) async throws -> String
 }
 
